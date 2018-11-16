781b2d917fc4e8b56669d34437e1b0b8d33a4ad4
Reddy Poreddy <reddy@reddymbp15.home>
2018-11-16 14:10:37 -0600

    step2

--- src/osutils.py
+++ src/osutils.py
@@ -1,2 +1,19 @@
+import datetime
 import os
-print "Hello " + os.getcwd()
+from datetime import date
+
+def diff_dates(date1, date2):
+    return abs(date2-date1).days
+
+d1 = date(2016,11,1)
+d2 = date(2017,9,23)
+result1 = diff_dates(d2, d1)
+print ('{} days between {} and {}'.format(result1, d1, d2))
+
+now = datetime.datetime.now()
+
+print ("CWD : " + os.getcwd())
+print (now.strftime("%Y-%m-%d %H:%M"))
+
+print ("Current date and time using isoformat:")
+print (now.isoformat())
