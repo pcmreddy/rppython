3a4bbdee7e9fd7bff06c662523352a0517a05ece
Reddy Poreddy <reddy@reddymbp15.home>
2018-11-19 21:03:28 -0600

    added angular examples

--- src/osutils.py
+++ src/osutils.py
@@ -1,13 +1,16 @@
-import datetime
-import os
+import datetime 
 from datetime import date
+
+import os
 
 def diff_dates(date1, date2):
     return abs(date2-date1).days
 
-d1 = date(2016,11,1)
-d2 = date(2017,9,23)
+d1 = date(2016, 11, 1)
+d2 = date(2017, 9, 23)
+
 result1 = diff_dates(d2, d1)
+
 print ('{} days between {} and {}'.format(result1, d1, d2))
 
 now = datetime.datetime.now()
